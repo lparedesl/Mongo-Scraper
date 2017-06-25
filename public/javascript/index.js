75430792e7a6e9792f91e2ece6c1f4f796463b7b
Luis Paredes <lparedesl@gmail.com>
2017-06-25 18:13:06 -0400

    Added home page and saved articles page

---
+++ public/javascript/index.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+    $("#scrape").click(function() {
+        $('#scrape-done').modal();
+    });
+
+    $("#view-notes").click(function() {
+        $('#article-notes').modal();
+    });
+});
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,15 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Express'
+        title: 'Mongo Scraper',
+        homepage: true
+    });
+});
+
+router.get('/saved-articles', function(req, res, next) {
+    res.render('index', {
+        title: 'Mongo Scraper | Saved Articles',
+        articles: true
     });
 });
 
